e5ad3fc895ae255f6fd2c9c22658a2bff01a1ac2
AjayLiu <ajayliu2928@gmail.com>
2021-02-08 11:40:18 -0800

    added start menu scene with working menu

---
+++ Assets/Scripts/StartMenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenuScript : MonoBehaviour
+{
+    public TextAsset creditTxt, howToTxt;
+    public GameObject panel;
+    Text panelText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelText = panel.GetComponentInChildren<Text>();
+        panel.SetActive(false);
+    }
+
+    public int gameSceneIndex;
+    public void PlayButtonPress()
+    {
+        SceneManager.LoadScene(gameSceneIndex);
+    }
+    public void CreditsButtonPress()
+    {
+        panel.SetActive(true);
+        panelText.text = creditTxt.text;
+    }
+    public void HowToPlayButtonPress()
+    {
+        panel.SetActive(true);
+        panelText.text = howToTxt.text;
+    }
+    public void OnXButtonPress()
+    {
+        panel.SetActive(false);
+    }
+
+
+}
